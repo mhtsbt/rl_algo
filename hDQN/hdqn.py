8e2b2b73b16a520162107300265812e5437f3f81
mhtsbt <matthias.hutsebaut@gmail.com>
2021-01-17 15:20:56 +0100

    controller training should work

--- hDQN/hdqn.py
+++ hDQN/hdqn.py
@@ -4,37 +4,56 @@ import numpy as np
 import torch
 import cv2
 import gym.spaces
-import matplotlib.pyplot as plt
+from torch.optim import Adam
+from torch.nn.functional import smooth_l1_loss
+from collections import deque
 
 
 class ReplayBuffer:
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, capacity):
+        self.capacity = capacity
         self.index = 0
+        self.size = 0
 
-        self.states = np.zeros(shape=(size, 84, 84*1), dtype=np.uint8)
-        self.next_states = np.zeros(shape=(size, 84, 84*1), dtype=np.uint8)
-        self.rewards = np.zeros((size, 1))
-        self.done = np.zeros((size, 1))
-        self.actions = np.zeros((size, 1), dtype=np.uint8)
+        self.states = np.zeros(shape=(self.capacity, 84, 84*2), dtype=np.uint8)
+        self.next_states = np.zeros(shape=(self.capacity, 84, 84*2), dtype=np.uint8)
+        self.rewards = np.zeros((self.capacity, 1))
+        self.non_terminal = np.zeros((self.capacity, 1))
+        self.actions = np.zeros((self.capacity, 1), dtype=np.uint8)
 
-    def store(self, state, next_state):
+    def store(self, state, next_state, action, reward, done):
         self.states[self.index] = state
-        self.states[self.index] = next_state
+        self.next_states[self.index] = next_state
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.non_terminal[self.index] = not done
 
-        self.index = (self.index+1) % self.size
-        print(self.index)
+        if self.index > self.size:
+            self.size = self.index
+
+        self.index = (self.index+1) % self.capacity
+
+    def get_samples(self, size):
+        idx = np.random.default_rng().choice(self.size, size=size, replace=False)
+
+        states = self.states[idx]
+        next_states = self.next_states[idx]
+        actions = self.actions[idx]
+        rewards = self.rewards[idx]
+        non_terminal = self.non_terminal[idx]
+
+        return states, next_states, actions, rewards, non_terminal
 
 
 class MetaController:
     def __init__(self):
         self.subgoals = []
         self.subgoals.append(SubGoal(name="middle-ladder", imgsize=84, width=3, height=3, originx=40, originy=48))
-        self.subgoals.append(SubGoal(name="top-left-door", imgsize=84, width=4, height=10, originx=13, originy=25))
-        self.subgoals.append(SubGoal(name="top-right-door", imgsize=84, width=4, height=10, originx=65, originy=25))
-        self.subgoals.append(SubGoal(name="bottom-left-ladder", imgsize=84, width=4, height=10, originx=10, originy=60))
+        #self.subgoals.append(SubGoal(name="top-left-door", imgsize=84, width=4, height=10, originx=13, originy=25))
+        #self.subgoals.append(SubGoal(name="top-right-door", imgsize=84, width=4, height=10, originx=65, originy=25))
+        #self.subgoals.append(SubGoal(name="bottom-left-ladder", imgsize=84, width=4, height=10, originx=10, originy=60))
         self.subgoals.append(SubGoal(name="bottom-right-ladder", imgsize=84, width=4, height=10, originx=69, originy=60))
-        self.subgoals.append(SubGoal(name="key", imgsize=84, width=3, height=3, originx=6, originy=40))
+        #self.subgoals.append(SubGoal(name="key", imgsize=84, width=3, height=3, originx=6, originy=40))
 
         self.action_space = gym.spaces.Discrete(n=len(self.subgoals))
         self.model = MetaControllerModel(n_options=self.action_space.n, device='cuda')
@@ -50,11 +69,74 @@ class MetaController:
 class Controller:
     def __init__(self, action_space):
         self.action_space = action_space
-        self.model = ControllerModel(n_actions=action_space.n, device='cuda')
-        self.buffer = ReplayBuffer(size=int(1e6))
+        self.device = "cuda"
 
-    def act(self, state, subgoal_mask):
-        return self.action_space.sample()
+        model_config = {"n_actions": action_space.n, "device": self.device}
+        self.model = ControllerModel(**model_config)
+        self.target_model = ControllerModel(**model_config)
+        self.sync_models()
+        self.optimizer = Adam(params=self.model.parameters(), lr=0.0000625)
+        self.criterion = smooth_l1_loss
+        self.min_eps = 0.01
+        self.eps_decay_step = (1-self.min_eps)/int(1e6)
+        self.eps = 1
+
+        self.buffer = ReplayBuffer(capacity=int(1e6))
+
+    def sync_models(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    @staticmethod
+    def process_state(state, subgoal_mask):
+        state = np.concatenate((state, subgoal_mask), axis=2)
+        return state
+
+    def greedy_action(self, state):
+        with torch.no_grad():
+            values = self.model(torch.tensor([state/255.0], dtype=torch.float, device=self.device))
+            action = values.argmax().item()
+            return action
+
+    def act(self, state):
+
+        if np.random.rand() < self.eps:
+            action = self.action_space.sample()
+        else:
+            action = self.greedy_action(state)
+
+        if self.eps > self.min_eps:
+            self.eps -= self.eps_decay_step
+
+        return action
+
+    def update(self):
+        states, next_states, actions, rewards, non_terminal = self.buffer.get_samples(size=32)
+
+        states = torch.tensor(states/255.0, device=self.device, dtype=torch.float).unsqueeze(1)
+        next_states = torch.tensor(next_states / 255.0, device=self.device, dtype=torch.float).unsqueeze(1)
+        actions = torch.tensor(actions, dtype=torch.long, device=self.device)
+        rewards = torch.tensor(rewards, device=self.device, dtype=torch.float)
+        non_terminal = torch.tensor(non_terminal, dtype=torch.long, device=self.device)
+
+        # get the current Q(s_t, a_t)
+        values = self.model(states)
+        qa_values = torch.gather(values, 1, actions)
+
+        with torch.no_grad():
+            future_value = self.target_model(next_states)
+            future_value, _ = future_value.max(dim=1)
+            future_value = future_value.unsqueeze(1)
+
+        # calculate the new expected action-values
+        new_qa = rewards + non_terminal*0.99*future_value
+
+        # calculate the loss
+        loss = self.criterion(input=qa_values, target=new_qa)
+
+        # update the network
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
 
 class SubGoal:
@@ -74,12 +156,12 @@ class SubGoal:
         return sum(sum(goal_viewport))
 
     def _get_mask(self):
-        goal = np.zeros((self.imgsize, self.imgsize), dtype=np.uint8)
+        goal = np.zeros((1, self.imgsize, self.imgsize), dtype=np.uint8)
 
         for row_index in range(0, self.imgsize):
             for col_index in range(0, self.imgsize):
                 if self.originy < row_index < self.originy+self.height and self.originx < col_index < self.originx + self.width:
-                    goal[row_index][col_index] = 255
+                    goal[0][row_index][col_index] = 255
         return goal
 
     def is_goal_reached(self, frame):
@@ -93,7 +175,6 @@ class SubGoal:
 
 
 class hDQN:
-
     def __init__(self):
         self.env = gym.make("MontezumaRevengeNoFrameskip-v4")
 
@@ -106,38 +187,51 @@ class hDQN:
         return state
 
     def run(self, n_steps):
-
         done = True
         subgoal_reached = False
         prev_env_state, prev_state, subgoal_mask, subgoal = None, None, None, None
 
-        for _ in range(n_steps):
+        subgoal_history = deque(maxlen=100)
 
+        for step_id in range(n_steps):
             if done:
                 self.env.reset()
                 # get the observation from the ale buffer (this is faster than converting the color image to grayscale)
                 prev_env_state = self.env.ale.getScreenGrayscale()
                 prev_state = self._process_state(prev_env_state)
-                subgoal_reached = True
-                print("Started new ep")
+
+                if not subgoal_reached:
+                    subgoal_history.append(0)
+
+                subgoal, subgoal_mask = self.meta_controller.sample_subgoal(state=prev_state)
+
+                print(f"steps: {step_id} success: {np.average(subgoal_history)}")
 
             if subgoal_reached:
                 subgoal, subgoal_mask = self.meta_controller.sample_subgoal(state=prev_state)
-                print(f"Selected new subgoal {subgoal}")
 
-            #cv2.imshow("render", prev_env_state)
-            #cv2.waitKey(1)
+            prev_controller_state = self.controller.process_state(prev_state, subgoal_mask)
 
-            action = self.controller.act(state=prev_state, subgoal_mask=subgoal_mask)
-
+            action = self.controller.act(state=prev_controller_state)
             _, reward, done, info = self.env.step(action=action)
 
             result_env_state = self.env.ale.getScreenGrayscale()
             result_state = self._process_state(result_env_state)
+            result_controller_state = self.controller.process_state(result_state, subgoal_mask)
 
             subgoal_reached = self.meta_controller.subgoal_validator(result_state, subgoal_id=subgoal)
 
-            self.controller.buffer.store(state=prev_state, next_state=result_state)
+            if subgoal_reached:
+                #print(f"subgoal {self.meta_controller.subgoals[subgoal].name} reached")
+                subgoal_history.append(1)
+
+            self.controller.buffer.store(state=prev_controller_state, next_state=result_controller_state, action=action, reward=subgoal_reached, done=subgoal_reached)
+
+            if step_id % int(80e3) == 0:
+                print("syncing models")
+                self.controller.sync_models()
+
+            if step_id > 100:
+                self.controller.update()
 
             prev_state = result_state
-            prev_env_state = result_env_state
--- hDQN/hdqn_models.py
+++ hDQN/hdqn_models.py
@@ -33,9 +33,9 @@ class MetaControllerModel(nn.Module):
             nn.Linear(in_features=512, out_features=n_options)
         )
 
-    def forward(self, state):
-        torch.tensor(state/255.0, dtype=torch.float, device=self.device)
+        self.to(device)
 
+    def forward(self, state):
         x = self.visual_pipeline(state)
         x = self.value_function(x)
         return x
@@ -44,7 +44,28 @@ class MetaControllerModel(nn.Module):
 class ControllerModel(nn.Module):
     def __init__(self, n_actions, device):
         super(ControllerModel, self).__init__()
-        pass
 
-    def forward(self):
-        pass
+        self.device = device
+
+        self.visual_pipeline = nn.Sequential(
+            nn.Conv2d(1, out_channels=32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1),
+            nn.ReLU(),
+            Flatten()
+        )
+
+        self.value_function = nn.Sequential(
+            nn.Linear(in_features=7616, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=n_actions)
+        )
+
+        self.to(device)
+
+    def forward(self, state):
+        x = self.visual_pipeline(state)
+        x = self.value_function(x)
+        return x
